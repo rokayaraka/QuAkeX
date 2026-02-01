45f1870eb9e1024cb3f7c4e45551ee105b6b4146
rokayaraka <rokayaraka251@gmail.com>
2026-02-01 18:58:55 +0600

    project and library setup

--- lib/myApp.dart
+++ lib/myApp.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      builder: EasyLoading.init(),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Hello, Earthquake App!'),
+        ),
+      ),
+    );
   }
 }
